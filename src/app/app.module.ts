9e7184af30dc87a4db350c2e9125da5d091778e5
Jairo Nicolás Gómez <jn.gomezm1@uniandes.edu.co>
2022-04-27 12:53:42 -0500

    Entrega final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SerieModule } from './serie/serie.module';
+
+
 
 @NgModule({
   declarations: [
@@ -10,7 +14,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SerieModule,
+    HttpClientModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
